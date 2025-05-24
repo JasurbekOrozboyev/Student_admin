51499869d665bc4cb0da5a7269392743d7028eae
JasurbekOrozboyev <orazboyev1jasurbek@gmail.com>
2025-05-24 09:31:03 +0500

    ###

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Student Admin',
+        short_name: 'StudentAdmin',
+        description: 'Student Admin web app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',  
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
